d81c4c9048ab8a808d180bb60de3cf931f0157a1
Masum Hossain <masum14092@gmail.com>
2022-01-07 23:41:32 +0600

    Update recording_utils.go

--- utils/recording_utils.go
+++ utils/recording_utils.go
@@ -126,8 +126,8 @@ func (rec *Recorder) Start() (string, error) {
 					"streamTypes": 2,
 					"channelType": 1,
 					"transcodingConfig": {
-						"height": 720,
-						"width": 1280,
+						"height": 540,
+						"width": 1200,
 						"bitrate": 2260,
 						"fps": 15,
 						"mixedVideoLayout": 1,
